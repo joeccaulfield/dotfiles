7761b7771e86bbe8e92538eefd03e6a72366eb54
Joe Caulfield <caulfield@theinstitutes.org>
2022-06-30 11:52:09 -0400

    Added conditional installs for terraform and omp

---
+++ programs/oh-my-posh-themes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download the themes
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm -f ~/.poshthemes/themes.zip
