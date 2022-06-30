16b9d5a3fbb57237980e4d8defb0107ed4c75758
Joe Caulfield <caulfield@theinstitutes.org>
2022-06-30 12:39:16 -0400

    corrected path used for programs scripts in setup-container.sh

--- setup-container.sh
+++ setup-container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install oh-my-posh and themes if not already installed
-[command -v oh-my-posh >/dev/null 2>&1] || ./oh-my-posh.sh
-[-d "~/.poshthemes"] || ./oh-my-posh-themes.sh
+[command -v oh-my-posh >/dev/null 2>&1] || ./programs/oh-my-posh.sh
+[-d "~/.poshthemes"] || ./programs/oh-my-posh-themes.sh
 
 # Create symlinks
 ./symlink.sh
