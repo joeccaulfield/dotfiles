c100bb456c11f67d11ce410735e2638958787f31
Joe Caulfield <caulfield@theinstitutes.org>
2022-06-30 15:17:23 -0400

    added quiet flag to unzip in setup-container.sh

--- setup-container.sh
+++ setup-container.sh
@@ -10,7 +10,7 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 # Download the themes
 mkdir ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip -o ~/.poshthemes/themes.zip -d ~/.poshthemes
+unzip -oq ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
 rm -f ~/.poshthemes/themes.zip
 
