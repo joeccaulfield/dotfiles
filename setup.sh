4e1fc98ec5fdc34739e639d3ee10788d499acf62
Joe Caulfield <caulfield@theinstitutes.org>
2022-06-30 10:53:25 -0400

    moved logic from setup.sh to install.sh for install scripts

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 ./symlink.sh
-if [ "${DF_APP_INS_ENABLE:-1}" = "1" ]; then ./aptinstall.sh; fi
-if [ "${DF_APP_INS_ENABLE:-1}" = "1" ]; then ./programs.sh; fi
-
-# Get all upgrades
-sudo apt upgrade -y
 
 # See our bash changes
 source ~/.bashrc
